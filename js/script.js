a7b59d71774a676f9174e5771526607f94f8bea4
Stanysmaev <yarik231988@gmail.com>
2023-04-13 20:01:45 +0300

    update

--- js/script.js
+++ js/script.js
@@ -56,3 +56,14 @@ const functionExpirienceChecker = (arr) => {
 
 const experienced = functionExpirienceChecker(array);
 console.log(experienced);
+
+function calcSum(a) {
+  return function sum(b) {
+    return (a += b);
+  };
+}
+const sum = calcSum(0);
+
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20));
